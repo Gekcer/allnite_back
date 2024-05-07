1c7d982ea14480146c0578ba6d09568d44805af2
Gekcer <gekcer@gmail.com>
2024-05-07 16:08:51 +0300

    фикс создания бд

--- db/create_tables.py
+++ db/create_tables.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, declarative_base
 
 engine = create_engine("sqlite:///all_nite_db.db", echo=True)
 
@@ -8,22 +7,22 @@ Base = declarative_base()
 
 
 class Bar(Base):
-    __tablename__ = 'bar'
+    __tablename__ = 'BAR'
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
-    vk_url = Column(Integer, nullable=True)
-    tg_url = Column(Integer, nullable=True)
-    inst_url = Column(Integer, nullable=True)
+    vk_url = Column(String, nullable=True)
+    tg_url = Column(String, nullable=True)
+    inst_url = Column(String, nullable=True)
 
 class ActivityInfo(Base):
-    __tablename__ = 'activity_info'
+    __tablename__ = 'ACTIVITYINFO'
 
     id = Column(Integer, primary_key=True)
-    bar_id = Column(Integer, ForeignKey('bar.id'))
+    bar_id = Column(Integer, ForeignKey('BAR.id'))
     media = Column(String, nullable=False)
-    description = Column(String)
-    date = Column(Date)
+    description = Column(String, nullable=True)
+    date = Column(Date, nullable=True)
 
     bar = relationship('Bar', order_by=Bar.id, back_populates='bar')
 
